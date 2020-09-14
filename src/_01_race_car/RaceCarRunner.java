8bda962aa47faee0c144e4568450acbb6667d3a8
Pallav <Pallav@DESKTOP-DVPQU53>
2020-09-14 13:31:23 -0700

    Finished RaceCar!

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -11,14 +11,19 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	RaceCar ice = new RaceCar("brand", 5);
 		// 2. Print the RaceCar's position in the race
-
+System.out.println(ice.getPositionInRace());
 		// 3. Crash the RaceCar
-		
+		ice.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+if(ice.damaged == true) {
+	ice.pit();
+}
 		// 5. Help the car move into first place.
-
+while (ice.getPositionInRace()>1) {
+	ice.overtake();
+}
 	}
 }
+
